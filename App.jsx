8a9a4db79b4b4376cdf24eda4234f721569be4b2
abhijithsn1744 <84909417+abhijithsn1744@users.noreply.github.com>
2021-06-07 12:35:05 +0530

    Add files via upload

---
+++ App.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import Header from "./header";
+import Note from "./Note";
+
+function App() {
+  return (
+    <div>
+      <Header />
+      <Note />
+      <Note />
+      <Note />
+    </div>
+  );
+}
+export default App;
